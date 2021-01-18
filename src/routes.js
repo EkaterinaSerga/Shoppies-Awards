f0a06eb2d0ab39bee91cdd2c9a29f22167d3a88f
Ekaterina Serga <ekaterina.a.serga@gmail.com>
2021-01-17 23:29:49 -0500

    feat: allow not logged in users search and nominate movies

--- src/routes.js
+++ src/routes.js
@@ -21,8 +21,9 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
+        <Route exact path="/search" component={Search} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={Signup} />
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
